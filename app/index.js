13b269e6e3afaace0f4e1f1a145585d5d37a9a8b
unknown <slavko.vuletic92@gmail.com>
2022-03-01 05:34:12 +0100

    yea

--- app/index.js
+++ app/index.js
@@ -1,25 +1,22 @@
 require("dotenv").config();
+const PORT = process.env.APP_DOCKER_PORT || 3000;
 
 const express = require("express");
 const app = express();
 
 const cors = require("cors");
 var corsOptions = {
-    origin: "http://localhost:3000"
+    origin: "http://localhost:"+PORT
 };
 
-
 app.use(cors(corsOptions));
-app.use(express.json());  // parse requests of content-type - application/json
-app.use(express.urlencoded({ extended: true }));  // parse requests of content-type - application/x-www-form-urlencoded
-
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));  
 
 app.get("/", async (req, res) => {
     return res.end("Welcome to V9_Application.");
 });
 
-// set port, listen for requests
-const PORT = process.env.APP_DOCKER_PORT || 3000;
 app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}.`);
 });
